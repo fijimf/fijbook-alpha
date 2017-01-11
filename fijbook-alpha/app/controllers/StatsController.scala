1812e3a5dac12b862c227075d612d873510339c4
Jim Frohnhofer <jim.frohnhofer@orchard-app.com>
2017-01-11 14:00:10 -0500

    Additonal logging

--- fijbook-alpha/app/controllers/StatsController.scala
+++ fijbook-alpha/app/controllers/StatsController.scala
@@ -11,6 +11,7 @@ import com.mohiva.play.silhouette.api.Silhouette
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics
 import play.api.Logger
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.json._
 import play.api.mvc.{Action, AnyContent, Controller}
 import utils.DefaultEnv
 
@@ -19,6 +20,8 @@ import scala.util.{Failure, Success}
 
 class StatsController @Inject()(val teamDao: ScheduleDAO, val statWriterService: StatisticWriterService, val silhouette: Silhouette[DefaultEnv], val messagesApi: MessagesApi) extends Controller with I18nSupport {
   val log = Logger(this.getClass)
+
+  import play.api.libs.json._
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -77,6 +80,27 @@ class StatsController @Inject()(val teamDao: ScheduleDAO, val statWriterService:
     }
   }
 
+  def viewStatApi(modelKey: String, statKey: String) = silhouette.UserAwareAction.async { implicit request =>
+    statWriterService.lookupModel(modelKey).flatMap(m => statWriterService.lookupStat(modelKey, statKey).map((m, _))) match {
+      case Some((model, stat)) => {
+        teamDao.loadSchedules().flatMap(ss => {
+          val sortedSchedules = ss.sortBy(s => -s.season.year)
+          sortedSchedules.headOption match {
+            case Some(sch) => {
+              teamDao.loadStatValues(statKey, modelKey).map(stats => {
+                val byDate = stats.groupBy(s => s.date)
+                val statContext = StatContext(model,stat,byDate, sch.teamsMap)
+                Ok(Json.toJson(statContext.asMap))
+              })
+            }
+            case None => Future.successful(Ok(Json.toJson(Map.empty[String, String])))
+          }
+        })
+      }
+      case None => Future.successful(Ok(Json.toJson(Map.empty[String, String])))
+    }
+  }
+
   def viewModel(modelKey: String) = silhouette.UserAwareAction.async { implicit request =>
     statWriterService.lookupModel(modelKey) match {
       case Some(model) => {
@@ -127,6 +151,33 @@ case class ModelContext(model: Model[_], stats: List[Stat[_]], xs: Map[LocalDate
 }
 
 case class StatContext(model: Model[_], stat: Stat[_], xs: Map[LocalDate, List[StatValue]], teamMap: Map[Long, Team]) {
+
+  def asMap = {
+    val jsString = JsString(latestDate.toString)
+    JsObject(Seq(
+      "model" -> JsObject(Seq(
+        "name" -> JsString(model.name),
+        "key" -> JsString(model.key)
+      )),
+      "stat" -> JsObject(Seq(
+        "name" -> JsString(stat.name),
+        "key" -> JsString(stat.key),
+        "higherIsBetter" -> JsBoolean(stat.higherIsBetter)
+      )),
+      "asOf" -> jsString,
+      "descriptiveStats" -> JsObject(Seq(
+        "mean" -> JsNumber(desc.getMean),
+        "var" -> JsNumber(desc.getVariance),
+        "min" -> JsNumber(desc.getMin),
+        "q1" -> JsNumber(desc.getPercentile(25.0)),
+        "med" -> JsNumber(desc.getPercentile(50.0)),
+        "q3" -> JsNumber(desc.getPercentile(75.0)),
+        "max" -> JsNumber(desc.getMax)
+      )),
+      "values" -> JsArray(latestValues.map { case (rk: Int, value: StatValue, team: Team) => JsObject(Seq("rank" -> JsNumber(rk), "value" -> JsNumber(value.value), "team" -> JsString(team.name), "teamKey" -> JsString(team.key))) }.toSeq)
+    ))
+  }
+
   def modelName = model.name
 
   def statName = stat.name
