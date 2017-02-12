aa1fafc351a55906855ae03ad52924c4767d5c12
Jim Frohnhofer <jim.frohnhofer@orchard-app.com>
2017-02-12 16:59:45 -0500

    Win/loss streak switcheroo

--- fijbook-alpha/app/com/fijimf/deepfij/stats/WonLost.scala
+++ fijbook-alpha/app/com/fijimf/deepfij/stats/WonLost.scala
@@ -52,8 +52,8 @@ case object WonLost extends Model[WonLostAccumulator]{
   val stats: List[Stat[WonLostAccumulator]] = List(
     Stat[WonLostAccumulator]("Wins", "wins", 0, higherIsBetter = true, _.wins),
     Stat[WonLostAccumulator]("Losses", "losses", 0, higherIsBetter = false, _.losses),
-    Stat[WonLostAccumulator]("Winning Streak", "wstreak", 0, higherIsBetter = true, _.lossStreak),
-    Stat[WonLostAccumulator]("Losing Streak", "lstreak", 0, higherIsBetter = false, _.winStreak),
+    Stat[WonLostAccumulator]("Winning Streak", "wstreak", 0, higherIsBetter = true, _.winStreak),
+    Stat[WonLostAccumulator]("Losing Streak", "lstreak", 0, higherIsBetter = false, _.lossStreak),
     Stat[WonLostAccumulator]("Winning Pct.", "wp", 0, higherIsBetter = true, _.winPct)
   )
   val key = "won-lost"
