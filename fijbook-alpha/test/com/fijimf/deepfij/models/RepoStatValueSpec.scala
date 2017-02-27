99d62c268c9a579d954f16aebb88a23b3d416567
Jim Frohnhofer <jim.frohnhofer@orchard-app.com>
2017-02-27 15:15:36 -0500

    Reformat

--- fijbook-alpha/test/com/fijimf/deepfij/models/RepoStatValueSpec.scala
+++ fijbook-alpha/test/com/fijimf/deepfij/models/RepoStatValueSpec.scala
@@ -1,6 +1,6 @@
 package com.fijimf.deepfij.models
 
-import java.time.{LocalDate, LocalDateTime}
+import java.time.LocalDate
 
 import com.fijimf.deepfij.models.dao.schedule.ScheduleDAO
 import org.scalatest.BeforeAndAfterEach
@@ -10,8 +10,8 @@ import org.scalatestplus.play._
 import play.api.test._
 import testhelpers.Injector
 
+import scala.concurrent.Await
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
 
 
 class RepoStatValueSpec extends PlaySpec with OneAppPerTest with BeforeAndAfterEach with ScalaFutures {
@@ -34,97 +34,97 @@ class RepoStatValueSpec extends PlaySpec with OneAppPerTest with BeforeAndAfterE
     }
 
     "be able to save baby statValues" in new WithApplication(FakeApplication()) {
-      val dates = 1.to(2).map(d=> LocalDate.now().plusDays(d)).toList
-      private val statValues = 1.to(35).flatMap(t=> dates.map(d=>StatValue(0L,"model","stat", t.toLong,d,0.123))).toList
+      val dates = 1.to(2).map(d => LocalDate.now().plusDays(d)).toList
+      private val statValues = 1.to(35).flatMap(t => dates.map(d => StatValue(0L, "model", "stat", t.toLong, d, 0.123))).toList
       Await.result(dao.saveStatValues(15, dates, List("model"), statValues), Duration.Inf)
-      assert(Await.result(dao.listStatValues, Duration.Inf).size==2*35)
+      assert(Await.result(dao.listStatValues, Duration.Inf).size == 2 * 35)
     }
     "be able to save large statValues" in new WithApplication(FakeApplication()) {
-      val dates = 1.to(60).map(d=> LocalDate.now().plusDays(d)).toList
-      private val statValues = 1.to(350).flatMap(t=> dates.flatMap(d=>List("Stat1", "Stat2", "Stat3").map(st=>StatValue(0L,"model",st, t.toLong,d,0.123)))).toList
+      val dates = 1.to(60).map(d => LocalDate.now().plusDays(d)).toList
+      private val statValues = 1.to(350).flatMap(t => dates.flatMap(d => List("Stat1", "Stat2", "Stat3").map(st => StatValue(0L, "model", st, t.toLong, d, 0.123)))).toList
       Await.result(dao.saveStatValues(15, dates, List("model"), statValues), Duration.Inf)
-      assert(Await.result(dao.listStatValues, Duration.Inf).size==60*350*3)
+      assert(Await.result(dao.listStatValues, Duration.Inf).size == 60 * 350 * 3)
     }
 
     "be able to save large statValues with a different batch size" in new WithApplication(FakeApplication()) {
-      val dates = 1.to(60).map(d=> LocalDate.now().plusDays(d)).toList
-      private val statValues = 1.to(350).flatMap(t=> dates.flatMap(d=>List("Stat1", "Stat2", "Stat3").map(st=>StatValue(0L,"model",st, t.toLong,d,0.123)))).toList
+      val dates = 1.to(60).map(d => LocalDate.now().plusDays(d)).toList
+      private val statValues = 1.to(350).flatMap(t => dates.flatMap(d => List("Stat1", "Stat2", "Stat3").map(st => StatValue(0L, "model", st, t.toLong, d, 0.123)))).toList
       Await.result(dao.saveStatValues(5, dates, List("model"), statValues), Duration.Inf)
-      assert(Await.result(dao.listStatValues, Duration.Inf).size==60*350*3)
+      assert(Await.result(dao.listStatValues, Duration.Inf).size == 60 * 350 * 3)
     }
 
     "be able to save empty list of statValues" in new WithApplication(FakeApplication()) {
-      val dates = 1.to(60).map(d=> LocalDate.now().plusDays(d)).toList
-      private val statValues = 1.to(350).flatMap(t=> dates.flatMap(d=>List.empty[String].map(st=>StatValue(0L,"model",st, t.toLong,d,0.123)))).toList
+      val dates = 1.to(60).map(d => LocalDate.now().plusDays(d)).toList
+      private val statValues = 1.to(350).flatMap(t => dates.flatMap(d => List.empty[String].map(st => StatValue(0L, "model", st, t.toLong, d, 0.123)))).toList
       Await.result(dao.saveStatValues(5, dates, List("model"), statValues), Duration.Inf)
       assert(Await.result(dao.listStatValues, Duration.Inf).isEmpty)
     }
 
     "be able to update large statValues" in new WithApplication(FakeApplication()) {
-      val dates = 1.to(60).map(d=> LocalDate.now().plusDays(d)).toList
-      private val statValues1 = 1.to(350).flatMap(t=> dates.flatMap(d=>List("Stat1", "Stat2", "Stat3").map(st=>StatValue(0L,"model",st, t.toLong,d,0.123)))).toList
+      val dates = 1.to(60).map(d => LocalDate.now().plusDays(d)).toList
+      private val statValues1 = 1.to(350).flatMap(t => dates.flatMap(d => List("Stat1", "Stat2", "Stat3").map(st => StatValue(0L, "model", st, t.toLong, d, 0.123)))).toList
       Await.result(dao.saveStatValues(5, dates, List("model"), statValues1), Duration.Inf)
       private val vals1 = Await.result(dao.listStatValues, Duration.Inf)
-      assert(vals1.size==60*350*3)
-      assert(vals1.forall(_.value>0.0))
+      assert(vals1.size == 60 * 350 * 3)
+      assert(vals1.forall(_.value > 0.0))
 
 
-      private val statValues2 = 1.to(350).flatMap(t=> dates.flatMap(d=>List("Stat1", "Stat2", "Stat3").map(st=>StatValue(0L,"model",st, t.toLong,d,-9.123)))).toList
+      private val statValues2 = 1.to(350).flatMap(t => dates.flatMap(d => List("Stat1", "Stat2", "Stat3").map(st => StatValue(0L, "model", st, t.toLong, d, -9.123)))).toList
       Await.result(dao.saveStatValues(5, dates, List("model"), statValues2), Duration.Inf)
       private val vals2 = Await.result(dao.listStatValues, Duration.Inf)
-      assert(vals2.size==60*350*3)
-      assert(vals2.forall(_.value<0.0))
+      assert(vals2.size == 60 * 350 * 3)
+      assert(vals2.forall(_.value < 0.0))
     }
 
 
     "be able to delete statValues " in new WithApplication(FakeApplication()) {
-      val dates = 1.to(60).map(d=> LocalDate.now().plusDays(d)).toList
-      private val statValues = 1.to(350).flatMap(t=> dates.flatMap(d=>List("Stat1", "Stat2", "Stat3").map(st=>StatValue(0L,"model",st, t.toLong,d,0.123)))).toList
+      val dates = 1.to(60).map(d => LocalDate.now().plusDays(d)).toList
+      private val statValues = 1.to(350).flatMap(t => dates.flatMap(d => List("Stat1", "Stat2", "Stat3").map(st => StatValue(0L, "model", st, t.toLong, d, 0.123)))).toList
       Await.result(dao.saveStatValues(5, dates, List("model"), statValues), Duration.Inf)
-      assert(Await.result(dao.listStatValues, Duration.Inf).size==60*350*3)
+      assert(Await.result(dao.listStatValues, Duration.Inf).size == 60 * 350 * 3)
 
-      Await.result(dao.deleteStatValues(List(LocalDate.now()),List("xxx-model")), Duration.Inf)
-      assert(Await.result(dao.listStatValues, Duration.Inf).size==60*350*3)
+      Await.result(dao.deleteStatValues(List(LocalDate.now()), List("xxx-model")), Duration.Inf)
+      assert(Await.result(dao.listStatValues, Duration.Inf).size == 60 * 350 * 3)
 
-      Await.result(dao.deleteStatValues(List(LocalDate.now().plusDays(61)),List("model")), Duration.Inf)
-      assert(Await.result(dao.listStatValues, Duration.Inf).size==60*350*3)
+      Await.result(dao.deleteStatValues(List(LocalDate.now().plusDays(61)), List("model")), Duration.Inf)
+      assert(Await.result(dao.listStatValues, Duration.Inf).size == 60 * 350 * 3)
 
-      Await.result(dao.deleteStatValues(List(LocalDate.now().plusDays(1)),List("model")), Duration.Inf)
+      Await.result(dao.deleteStatValues(List(LocalDate.now().plusDays(1)), List("model")), Duration.Inf)
       private val result = Await.result(dao.listStatValues, Duration.Inf)
-      assert(result.size==59*350*3)
+      assert(result.size == 59 * 350 * 3)
     }
 
     "be able to load statValues from model" in new WithApplication(FakeApplication()) {
-      val dates = 1.to(60).map(d=> LocalDate.now().plusDays(d)).toList
-      private val statValues = 1.to(350).flatMap(t=> dates.flatMap(d=>List("Stat1", "Stat2", "Stat3").map(st=>StatValue(0L,"model",st, t.toLong,d,0.123)))).toList
+      val dates = 1.to(60).map(d => LocalDate.now().plusDays(d)).toList
+      private val statValues = 1.to(350).flatMap(t => dates.flatMap(d => List("Stat1", "Stat2", "Stat3").map(st => StatValue(0L, "model", st, t.toLong, d, 0.123)))).toList
       Await.result(dao.saveStatValues(5, dates, List("model"), statValues), Duration.Inf)
-      assert(Await.result(dao.listStatValues, Duration.Inf).size==60*350*3)
+      assert(Await.result(dao.listStatValues, Duration.Inf).size == 60 * 350 * 3)
       private val result = Await.result(dao.loadStatValues("model"), Duration.Inf)
-      assert(result.size == 60*350*3)
+      assert(result.size == 60 * 350 * 3)
     }
 
     "be able to load statValues from model and stat key" in new WithApplication(FakeApplication()) {
-      val dates = 1.to(60).map(d=> LocalDate.now().plusDays(d)).toList
-      private val statValues = 1.to(350).flatMap(t=> dates.flatMap(d=>List("Stat1", "Stat2", "Stat3").map(st=>StatValue(0L,"model",st, t.toLong,d,0.123)))).toList
+      val dates = 1.to(60).map(d => LocalDate.now().plusDays(d)).toList
+      private val statValues = 1.to(350).flatMap(t => dates.flatMap(d => List("Stat1", "Stat2", "Stat3").map(st => StatValue(0L, "model", st, t.toLong, d, 0.123)))).toList
       Await.result(dao.saveStatValues(5, dates, List("model"), statValues), Duration.Inf)
-      assert(Await.result(dao.listStatValues, Duration.Inf).size==60*350*3)
+      assert(Await.result(dao.listStatValues, Duration.Inf).size == 60 * 350 * 3)
       private val result = Await.result(dao.loadStatValues("Stat3", "model"), Duration.Inf)
-      assert(result.size == 60*350)
+      assert(result.size == 60 * 350)
     }
 
     "not be able to load missing statValues from model " in new WithApplication(FakeApplication()) {
-      val dates = 1.to(60).map(d=> LocalDate.now().plusDays(d)).toList
-      private val statValues = 1.to(350).flatMap(t=> dates.flatMap(d=>List("Stat1", "Stat2", "Stat3").map(st=>StatValue(0L,"model",st, t.toLong,d,0.123)))).toList
+      val dates = 1.to(60).map(d => LocalDate.now().plusDays(d)).toList
+      private val statValues = 1.to(350).flatMap(t => dates.flatMap(d => List("Stat1", "Stat2", "Stat3").map(st => StatValue(0L, "model", st, t.toLong, d, 0.123)))).toList
       Await.result(dao.saveStatValues(5, dates, List("model"), statValues), Duration.Inf)
-      assert(Await.result(dao.listStatValues, Duration.Inf).size==60*350*3)
+      assert(Await.result(dao.listStatValues, Duration.Inf).size == 60 * 350 * 3)
       private val result = Await.result(dao.loadStatValues("model-xxx"), Duration.Inf)
       assert(result.isEmpty)
     }
     "not be able to load missing statValues from model and stat key " in new WithApplication(FakeApplication()) {
-      val dates = 1.to(60).map(d=> LocalDate.now().plusDays(d)).toList
-      private val statValues = 1.to(350).flatMap(t=> dates.flatMap(d=>List("Stat1", "Stat2", "Stat3").map(st=>StatValue(0L,"model",st, t.toLong,d,0.123)))).toList
+      val dates = 1.to(60).map(d => LocalDate.now().plusDays(d)).toList
+      private val statValues = 1.to(350).flatMap(t => dates.flatMap(d => List("Stat1", "Stat2", "Stat3").map(st => StatValue(0L, "model", st, t.toLong, d, 0.123)))).toList
       Await.result(dao.saveStatValues(5, dates, List("model"), statValues), Duration.Inf)
-      assert(Await.result(dao.listStatValues, Duration.Inf).size==60*350*3)
+      assert(Await.result(dao.listStatValues, Duration.Inf).size == 60 * 350 * 3)
       private val result = Await.result(dao.loadStatValues("model", "Stat-999"), Duration.Inf)
       assert(result.isEmpty)
     }
@@ -134,17 +134,3 @@ class RepoStatValueSpec extends PlaySpec with OneAppPerTest with BeforeAndAfterE
 
 
 }
-
-
-/*
-  def listStatValues: Future[List[StatValue]]
-
-  def deleteStatValues(dates: List[LocalDate], model: List[String]): Future[Unit]
-
-  def saveStatValues(batchSize: Int, dates: List[LocalDate], model: List[String], stats: List[StatValue]): Unit
-
-  def loadStatValues(statKey: String, modelKey: String): Future[List[StatValue]]
-
-  def loadStatValues(modelKey: String): Future[List[StatValue]]
-
- */
