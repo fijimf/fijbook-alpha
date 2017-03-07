b751910cbf039cd2d4fb32ca9b1b5e1e6502be8b
Jim Frohnhofer <jim.frohnhofer@orchard-app.com>
2017-03-06 23:42:36 -0500

    Bug fix on daily game scrape.

--- fijbook-alpha/app/com/fijimf/deepfij/stats/predictor/StatValueGameFeatureMapper.scala
+++ fijbook-alpha/app/com/fijimf/deepfij/stats/predictor/StatValueGameFeatureMapper.scala
@@ -1,17 +1,26 @@
 package com.fijimf.deepfij.stats.predictor
 
+import java.time.LocalDate
+
 import com.fijimf.deepfij.models.dao.schedule.ScheduleDAO
-import com.fijimf.deepfij.models.{Game, Result}
+import com.fijimf.deepfij.models.{Game, Result, StatValue}
 import org.apache.mahout.math.{DenseVector, Vector}
 import play.api.Logger
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
+sealed trait FeatureNormalization
+case object FeatureNormalizationZScoreByDate
+case object FeatureNormalizationZScoreByPop
+case object FeatureNormalizationMinMaxByDate
+case object FeatureNormalizationMinMaxPop
+case object FeatureNormalizationNone
+
 case class StatValueGameFeatureMapper(statKey: String, modelKey: String, dao: ScheduleDAO) extends FeatureMapper[(Game, Option[Result])] {
   val logger = Logger(this.getClass)
   import scala.concurrent.ExecutionContext.Implicits.global
-  val values = Await.result(dao.loadStatValues(statKey, modelKey).map(sv => {
+  val values: Map[LocalDate, Map[Long, StatValue]] = Await.result(dao.loadStatValues(statKey, modelKey).map(sv => {
     logger.info(s"Loaded ${sv.size} model values")
     sv.groupBy(_.date).mapValues(_.map(s => s.teamID -> s).toMap)
   }), Duration.Inf)
