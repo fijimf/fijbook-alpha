d5cada8cda644688fd3fbc1adb30adbaf6579811
Jim Frohnhofer <jim.frohnhofer@orchard-app.com>
2017-10-22 16:29:55 -0400

    Fix games by date to handle previous seasons

--- fijbook-alpha/app/controllers/GamesController.scala
+++ fijbook-alpha/app/controllers/GamesController.scala
@@ -8,8 +8,9 @@ import com.fijimf.deepfij.models.dao.schedule.ScheduleDAO
 import com.fijimf.deepfij.models.services.UserService
 import com.fijimf.deepfij.models.{Game, GprCohort}
 import com.mohiva.play.silhouette.api.Silhouette
+import com.mohiva.play.silhouette.api.actions.UserAwareRequest
 import play.api.libs.json.{JsArray, Json}
-import play.api.mvc.{BaseController, ControllerComponents}
+import play.api.mvc.{AnyContent, BaseController, ControllerComponents}
 import utils.DefaultEnv
 
 import scala.concurrent.ExecutionContext
@@ -33,24 +34,24 @@ val predictors = List(
     val tomorrow = today.plusDays(1)
     val yesterday = today.minusDays(1)
     teamDao.loadSchedules().map(ss => {
-      val sortedSchedules = ss.sortBy(s => -s.season.year)
-      val sch = sortedSchedules.headOption
+      val sch = ss.find(_.season.dates.contains(today))
       val todayGames = sch.map(_.games.filter(_.date == today)).getOrElse(List.empty[Game])
-      Ok(views.html.gamelist(rs.identity, today, sch, todayGames, yesterday, tomorrow, modelKey.getOrElse("naive-regression"),predictors))
+      Ok(views.html.gamelist(rs.identity, today, sch, todayGames, yesterday, tomorrow, modelKey.getOrElse("naive-regression"), predictors))
     })
   }
 
   def gamesByDate(yyyymmdd:String, modelKey:Option[String]) = silhouette.UserAwareAction.async { implicit rs =>
-    val today = LocalDate.parse(yyyymmdd, DateTimeFormatter.BASIC_ISO_DATE)
-     val tomorrow = today.plusDays(1)
+    val today= LocalDate.parse(yyyymmdd, DateTimeFormatter.BASIC_ISO_DATE)
+    val tomorrow = today.plusDays(1)
     val yesterday = today.minusDays(1)
     teamDao.loadSchedules().map(ss => {
-      val sortedSchedules = ss.sortBy(s => -s.season.year)
-      val sch = sortedSchedules.headOption
+      val sch = ss.find(_.season.dates.contains(today))
       val todayGames = sch.map(_.games.filter(_.date == today)).getOrElse(List.empty[Game])
+      Ok(views.html.gamelist(rs.identity, today, sch, todayGames, yesterday, tomorrow, modelKey.getOrElse("naive-regression"), predictors))
+    })}
 
-      Ok(views.html.gamelist(rs.identity, today, sch, todayGames, yesterday, tomorrow,modelKey.getOrElse("naive-regression"),predictors))
-    })
+  private def handleDateRequest(today: LocalDate, modelKey:Option[String], rs:UserAwareRequest[DefaultEnv, AnyContent]) = {
+
   }
 
   def gamesApi(yyyymmdd:String, modelKey:Option[String]) = silhouette.UserAwareAction.async { implicit rs =>
