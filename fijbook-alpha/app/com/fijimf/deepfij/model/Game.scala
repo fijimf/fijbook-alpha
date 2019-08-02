0cb3522df271161628b3157d6764540bc359f852
fijimf <fijimf@gmail.com>
2019-08-02 15:28:39 -0400

    Dooooobifying~!!!!!

--- fijbook-alpha/app/com/fijimf/deepfij/model/Game.scala
+++ fijbook-alpha/app/com/fijimf/deepfij/model/Game.scala
@@ -18,8 +18,6 @@ case class Game
   datetime: LocalDateTime,
   location: Option[String],
   isNeutralSite: Boolean,
-  result:Option[Result],
-  tournamentData: Option[TournamentData],
   sourceKey: String,
   updatedAt: LocalDateTime,
   updatedBy: String
@@ -43,8 +41,8 @@ object Game {
            datetime TIMESTAMP NOT NULL,
            location VARCHAR(48) NULL,
            is_neutral_site BOOLEAN NOT NULL,
-           source_ey VARCHAR(36) NOT NULL,
-           updated_att TIMESTAMP NOT NULL,
+           source_key VARCHAR(36) NOT NULL,
+           updated_at TIMESTAMP NOT NULL,
            updated_by VARCHAR(36) NOT NULL);
          CREATE UNIQUE INDEX ON game(season_id, date, home_team_id, away_team_id);
          CREATE INDEX ON game(season_id, date);
@@ -56,13 +54,8 @@ object Game {
 
     val select=
       fr"""SELECT
-             g.id, g.season_id, g.home_team_id, g.away_team_id, g.date, g.datetime, g.location, g.is_neutral_site
-             r.id, r.game_id, r.home_score, r.away_score, r.periods, r.updated_at, r.updated_by,
-             t.id, t.game_id, t.tournament_key, t.home_team_seed, t.away_team_seed, t.updated_at, t.updated_by,
-             g.source_key, g.updated_at, g.updated_by
+             g.id, g.season_id, g.home_team_id, g.away_team_id, g.date, g.datetime, g.location, g.is_neutral_site, g.source_key, g.updated_at, g.updated_by
            FROM game g
-           LEFT OUTER JOIN result r ON r.game_id=g.id
-           LEFT OUTER JOIN tournament_data t ON t.game_id=g.id
            """
     val delete=fr"""DELETE FROM game"""
   }
